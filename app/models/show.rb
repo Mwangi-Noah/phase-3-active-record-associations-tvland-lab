aeeab61a97d09294e71d46725d06609f51eb66de
Noah <noahmwangi@gmail.com>
2023-06-06 17:35:28 +0300

    Completed Assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+    has_many :characters, dependent: :destroy
+    belongs_to :network
   
+    def show_characters
+        Character.where(show_id: id)
+    end
+
+    def actors_list
+        characters.includes(:actor).map { |character| character.actor.full_name }
+    end  
 end
